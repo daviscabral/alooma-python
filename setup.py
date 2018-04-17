e2e395834f919e8b790e5089295de187db982441
yuvalb <yuval@alooma.io>
2018-04-17 20:37:57 +0300

    Fix setup.py file

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from distutils.core import setup
 try:
       from pip.req import parse_requirements
 except ImportError:
@@ -8,14 +9,13 @@ install_reqs = parse_requirements("requirements.txt", session=False)
 
 reqs = [str(ir.req) for ir in install_reqs]
 
-from distutils.core import setup
-
-setup(name='alooma',
-      version='0.3.21',
-      description='Alooma python API',
-      author='Yonatan Kiron',
-      author_email='yonatan@alooma.io',
-      packages=['alooma'],
-      install_requires=reqs,
-      keywords=['alooma']
-      )
+setup(
+    name='alooma',
+    version='0.3.21',
+    description='Alooma python API',
+    author='Alooma',
+    author_email='support@alooma.io',
+    packages=['alooma'],
+    install_requires=reqs,
+    keywords=['alooma', 'api']
+)
