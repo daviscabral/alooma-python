a854e0c2c31e76ae8428fe253659b985d002511b
Ram Amar <amar.rami@gmail.com>
2018-05-08 12:31:38 +0300

    reqs.txt: remove paramiko , bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='alooma',
-    version='0.3.21',
+    version='0.3.22',
     description='Alooma python API',
     author='Alooma',
     author_email='support@alooma.io',
